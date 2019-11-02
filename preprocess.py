e749487c5a40b5ca2fda18d4a978b93d0cb74bfb
Sanchit <sanchitahuja205@gmail.com>
2019-11-02 17:47:29 +0530

    Minor changes

--- preprocess.py
+++ preprocess.py
@@ -15,16 +15,16 @@ def get_sentence(doc):
 
 
 #English data
-filename1 = 'English.txt'
-english = load_doc(filename1)
-english_sentences = get_sentence(english)
-print("English corpus: %d" %(len(english_sentences)))
+# filename1 = 'English.txt'
+# english = load_doc(filename1)
+# english_sentences = get_sentence(english)
+# print("English corpus: %d" %(len(english_sentences)))
 
-#Dutch data
-filename2 = 'Dutch.txt'
-dutch = load_doc(filename2)
-dutch_sentences = get_sentence(dutch)
-print("Dutch corpus: %d" %(len(dutch_sentences)))
+# #Dutch data
+# filename2 = 'Dutch.txt'
+# dutch = load_doc(filename2)
+# dutch_sentences = get_sentence(dutch)
+# print("Dutch corpus: %d" %(len(dutch_sentences)))
 
 def cleaned_corpus(lines):
     clean_corpus =  []
@@ -72,21 +72,22 @@ def vocab_cnt(sentences):
 #Removing words
 def remove_voc(voc,threshold):
     saved = [word for word,cnt in voc.items() if cnt >= threshold]
-    return list(set(saved))
+    return (set(saved))
 
 
 #Updating the data set
-def update_data(sentences,voc):
+def update_dataset(sentences, vocab):
     new_sentences = []
-    for l in sentences:
+    for line in sentences:
         new_words = []
-        for t in l.split():
-            if t in voc:
+        for t in line.split():
+            if t in vocab:
                 new_words.append(t)
             else:
-                new_words.append('uuo') #Here we are replacing the words with less frequency with a placeholder
-        new_sentence = ' '.join(new_words)
-        new_sentences.append(new_sentence)
+                new_words.append('uup')
+        new_words = [word for word in new_words if word !='s']
+        new_line = ' '.join(new_words)
+        new_sentences.append(new_line)
     return new_sentences
 
 filename3 = 'English_cleaned.pkl'
@@ -97,15 +98,15 @@ lines_dutch = load(open(filename4,'rb'))
 vocab_eng = vocab_cnt(lines_eng)
 new_vocab_eng = remove_voc(vocab_eng,4)
 print('Old vocab size eng: %d New vocab size eng: %d'%(len(vocab_eng),len(new_vocab_eng)))
-new_data_eng = update_data(lines_eng,new_vocab_eng)
+new_data_eng = update_dataset(lines_eng,new_vocab_eng)
 for i in range(10):
     print(new_data_eng[i])
-# dump(new_data_eng,open('English_cleaned_voc.pkl','wb'))
+dump(new_data_eng,open('English_cleaned_voc.pkl','wb'))
 print("Saved english!")
 
 vocab_dutch = vocab_cnt(lines_dutch)
 new_vocab_dutch = remove_voc(vocab_dutch,4)
 print('Old vocab size dutch: %d New vocab size dutch %d'%(len(vocab_dutch),len(new_vocab_dutch)))
-new_data_dutch = update_data(lines_dutch,new_vocab_dutch)
-# dump(new_data_dutch,open('Dutch_cleaned_voc.pkl','wb'))
+new_data_dutch = update_dataset(lines_dutch,new_vocab_dutch)
+dump(new_data_dutch,open('Dutch_cleaned_voc.pkl','wb'))
 print("Saved dutch!")
