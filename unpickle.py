3f9f59059cdb59cb82a6d4a5674690ff4597a3da
Hemanth V. Alluri <hdrive1999@gmail.com>
2019-11-02 23:33:44 +0530

    unpickle: Add a CLI tool to convert pickled files into ASCII text files.

---
+++ unpickle.py
@@ -0,0 +1,17 @@
+import pickle
+import argparse
+
+def unpickle(filename):
+    """ Take a .pkl (binary) file, load it into python and print it back in ASCII. """
+    output_filename = ".".join(filename.split(".")[:-1]) + ".txt"
+    unpickled_data = ""
+    with open(filename, "rb") as binary_data:
+        unpickled_data = pickle.load(binary_data)
+    with open(output_filename, "w+") as f:
+        f.write("\n".join(unpickled_data))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="file to unpickle")
+    args = parser.parse_args()
+    unpickle(args.filename)
