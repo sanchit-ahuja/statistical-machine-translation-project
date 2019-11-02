e3cf248110cd93c6f0ff3f2769dbc938f845c377
Hemanth V. Alluri <hdrive1999@gmail.com>
2019-11-02 23:32:45 +0530

    preprocess: Clean up the code, add comments, and add a CLI.

--- preprocess.py
+++ preprocess.py
@@ -1,82 +1,34 @@
-import string
 import re
-from unicodedata import normalize
-from pickle import dump,load
+import string
+import pickle
+import argparse
+from typing import List, Dict
 from collections import Counter
-
-def load_doc(filename):
-    file = open(filename)
-    text = file.read()
-    file.close()
-    return text
-
-def get_sentence(doc):
-    return doc.strip().split('\n')
+from unicodedata import normalize
 
 
-#English data
-# filename1 = 'English.txt'
-# english = load_doc(filename1)
-# english_sentences = get_sentence(english)
-# print("English corpus: %d" %(len(english_sentences)))
+def get_sentences_from_document(filename: str) -> List[str]:
+    """ Load the target text file and return a list containing every sentence in it. """
+    with open(filename, "r") as file:
+        raw_text = file.read()
+        sentences = raw_text.strip().split("\n")
+    return sentences
 
-# #Dutch data
-# filename2 = 'Dutch.txt'
-# dutch = load_doc(filename2)
-# dutch_sentences = get_sentence(dutch)
-# print("Dutch corpus: %d" %(len(dutch_sentences)))
-
-def cleaned_corpus(lines):
-    clean_corpus =  []
-    #filter printable chars
-    re_pat = re.compile('[^%s]' % re.escape(string.printable))
-    #making a transtable to remove punctuation chars
-    table = str.maketrans('','',string.punctuation)
-    for line in lines:
-        line = normalize('NFD',line).encode('ascii','ignore') #Normalizing chars such as o` and o:
-        line = line.decode('UTF-8')
-        line = line.split() #tokenize on whitespaces
-        line = [word.lower() for word in line] #lowercase all the words
-        line = [word.translate(table) for word in line] #remove all the punctuation marks
-        line = [re_pat.sub('',word) for word in line] #remove non-printable chars
-        line = [word for word in line if word.isalpha()] #remove numbers
-        clean_corpus.append(' '.join(line))
-    return clean_corpus
-
-# english_cleaned = clean_corpus(english_sentences)
-# dump(english_cleaned,open('English_cleaned.pkl','wb'))
-# dutch_cleaned = cleaned_corpus(dutch_sentences)
-# dump(dutch_cleaned,open('Dutch_cleaned.pkl','wb'))
-
-def vocab_cnt(sentences):
+def get_vocabulary_count(sentences: List[str]) -> Dict[str, int]:
+    """ Return a mapping: {unique_word -> number_of_occurrences} based on the given sentences. """
     voc = Counter()
-    for l in sentences:
-        words = l.split()
+    for sentence in sentences:
+        words = sentence.split()
         voc.update(words)
     return voc
 
-
-
-# d = (vocab_cnt(lines_eng))
-# c = 0
-# for i in d:
-#     print(i)
-#     if c == 10:
-#         break
-#     c += 1
-
-# print('English vocab',(vocab_cnt(lines_eng)))
-# print('Dutch vocab',(vocab_cnt(lines_dutch)))
-
-
-#Removing words
-def remove_voc(voc,threshold):
-    saved = [word for word,cnt in voc.items() if cnt >= threshold]
-    return (set(saved))
-
+def reduce_vocab(vocab, threshold):
+    """ Return a set of words which have occurred at least the `threshold` number of times. """
+    return set([word for word, cnt in vocab.items() if cnt >= threshold])
 
 #Updating the data set
 def update_dataset(sentences, vocab):
+    """ Take the original sentences and then use the cleaned and reduced vocabulary to update them. """
     new_sentences = []
     for line in sentences:
         new_words = []
@@ -84,29 +36,49 @@ def update_dataset(sentences, vocab):
             if t in vocab:
                 new_words.append(t)
             else:
-                new_words.append('uup')
-        new_words = [word for word in new_words if word !='s']
-        new_line = ' '.join(new_words)
+                new_words.append("uup")
+        new_words = [word for word in new_words if word !="s"]
+        new_line = " ".join(new_words)
         new_sentences.append(new_line)
     return new_sentences
 
-filename3 = 'English_cleaned.pkl'
-lines_eng = load(open(filename3, 'rb'))
-filename4 = 'Dutch_cleaned.pkl'
-lines_dutch = load(open(filename4,'rb'))
+def clean_sentences(lines: List[str]) -> List[str]:
+    """ Normalize/clean the input lines by:
+            1. Converting phonetic Latin characters to their simple English form. E.g. o` -> o
+            2. Making all words lowercase.
+            3. Removing punctuation characters.
+            4. Removing all non-printable characters.
+            5. Removing all numeric characters. """
+    cleaned_sentences =  []
+    re_pat = re.compile("[^%s]" % re.escape(string.printable))  # filter printable chars
+    table = str.maketrans("", "", string.punctuation)
+    # `table` is a dictionary mapping the ascii value of each punctutation character to None.
+    # We use this to remove punctutation characters in the code below.
+    for line in lines:
+        line = normalize("NFD", line).encode("ascii", "ignore").decode("UTF-8")  # normalizing chars such as o` and o.
+        words = line.split()  # tokenize on whitespaces
+        lowercase_words = [word.lower() for word in words]  # lowercase all the words
+        punctuationless_words = [word.translate(table) for word in lowercase_words]  # remove all the punctuation marks
+        printable_words = [re_pat.sub("", word) for word in punctuationless_words]  # remove non-printable chars
+        non_numeric_words = [word for word in printable_words if word.isalpha()]  # remove numbers
+        cleaned_sentences.append(" ".join(non_numeric_words))
+    return cleaned_sentences
 
-vocab_eng = vocab_cnt(lines_eng)
-new_vocab_eng = remove_voc(vocab_eng,4)
-print('Old vocab size eng: %d New vocab size eng: %d'%(len(vocab_eng),len(new_vocab_eng)))
-new_data_eng = update_dataset(lines_eng,new_vocab_eng)
-for i in range(10):
-    print(new_data_eng[i])
-dump(new_data_eng,open('English_cleaned_voc.pkl','wb'))
-print("Saved english!")
+def preprocess_file(input_filename, output_filename, vocab_reduction_threshold=4):
+    """ Take one of the original input files and then completely preprocess it. """
+    # TODO: Add a check to skip steps if cleaned data exists.
+    raw_sentences = get_sentences_from_document(input_filename)
+    cleaned_sentences = clean_sentences(raw_sentences)
+    pickle.dump(cleaned_sentences, open(output_filename + ".pkl", "wb"))
+    vocab = get_vocabulary_count(cleaned_sentences)
+    reduced_vocab = reduce_vocab(vocab, vocab_reduction_threshold)
+    new_data = update_dataset(cleaned_sentences, reduced_vocab)
+    pickle.dump(new_data, open(output_filename + ".reduced.pkl", "wb"))
 
-vocab_dutch = vocab_cnt(lines_dutch)
-new_vocab_dutch = remove_voc(vocab_dutch,4)
-print('Old vocab size dutch: %d New vocab size dutch %d'%(len(vocab_dutch),len(new_vocab_dutch)))
-new_data_dutch = update_dataset(lines_dutch,new_vocab_dutch)
-dump(new_data_dutch,open('Dutch_cleaned_voc.pkl','wb'))
-print("Saved dutch!")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="path of file to process.", default="Input.txt")
+    parser.add_argument("-o", "--output", help="path of file to write processed data to process without an extension.", default="Output.txt")
+    parser.add_argument("-vrt", "--vocab_reduction_threshold", help="set the vocab reduction threshold.", default=4)
+    args = parser.parse_args()
+    preprocess_file(args.input, args.output, args.vocab_reduction_threshold)
