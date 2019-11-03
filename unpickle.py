ef4017e2f769165cabf3922b990925358ea7449e
Hemanth V. Alluri <hdrive1999@gmail.com>
2019-11-03 11:45:43 +0530

    unpickle: Seperate the unpickling to list logic from writing to file.
    Also, fix the type hinting errors.

--- unpickle.py
+++ unpickle.py
@@ -1,17 +1,20 @@
 import pickle
 import argparse
+from typing import List
 
-def unpickle(filename: str) -> None:
-    """ Take a .pkl (binary) file, load it into python and print it back in ASCII. """
-    output_filename = ".".join(filename.split(".")[:-1]) + ".txt"
-    unpickled_data = ""
+def unpickle(filename: str) -> List[str]:
+    """ Take a .pkl (binary) file, load it into python and print return the equivalent
+    list of ASCII strings. """
+    unpickled_data = []  # type: List[str]
     with open(filename, "rb") as binary_data:
         unpickled_data = pickle.load(binary_data)
-    with open(output_filename, "w+") as f:
-        f.write("\n".join(unpickled_data))
+    return unpickled_data
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="file to unpickle")
     args = parser.parse_args()
-    unpickle(args.filename)
+    unpickled_data = unpickle(args.filename)
+    output_filename = ".".join(args.filename.split(".")[:-1]) + ".txt"
+    with open(output_filename, "w+") as f:
+        f.write("\n".join(unpickled_data))
