c737dfeadafd8e7b5212af00f6e002492517bb73
Hemanth V. Alluri <hdrive1999@gmail.com>
2019-11-04 21:13:14 +0530

    unpickle: Extend unpickling functionality to dictionaries.
    This will be useful for when we want to view training probabilities 
    tables after running the model1 engine.

--- unpickle.py
+++ unpickle.py
@@ -1,11 +1,12 @@
+import json
 import pickle
 import argparse
-from typing import List
+from typing import List, Dict, Union
 
-def unpickle(filename: str) -> List[str]:
+def unpickle(filename: str) -> Union[List[str], Dict[str, Dict[str, float]]]:
     """ Take a .pkl (binary) file, load it into python and print return the equivalent
-    list of ASCII strings. """
-    unpickled_data = []  # type: List[str]
+    list of ASCII strings or equivalent dictionary. """
+    unpickled_data = None
     with open(filename, "rb") as binary_data:
         unpickled_data = pickle.load(binary_data)
     return unpickled_data
@@ -15,6 +16,11 @@ if __name__ == "__main__":
     parser.add_argument("filename", help="file to unpickle")
     args = parser.parse_args()
     unpickled_data = unpickle(args.filename)
+    import pdb; pdb.set_trace()
     output_filename = ".".join(args.filename.split(".")[:-1]) + ".txt"
     with open(output_filename, "w+") as f:
-        f.write("\n".join(unpickled_data))
+        if type(unpickled_data) == List:
+            f.write("\n".join(unpickled_data))
+        elif type(unpickled_data) == Dict:
+            f.write(json.dumps(unpickled_data, indent=4, sort_keys=True))
+        f.close()
