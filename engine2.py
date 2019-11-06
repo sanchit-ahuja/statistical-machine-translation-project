a7e59fbfaf4bd357d1c7a7168aa45bdf8c05eec1
Sanchit <sanchitahuja205@gmail.com>
2019-11-06 20:00:18 +0530

    few more changes

--- engine2.py
+++ engine2.py
@@ -7,13 +7,14 @@ from unpickle import unpickle
 from engine import get_vocab,converged,engine,printv,write_back_data
 
 
-def engine2(dutch_sentences, english_sentences,max_iterations = 5):
+def engine2(dutch_sentences, english_sentences):
     a_dict = defaultdict(float)
     translation_table_prev = unpickle("translation_probabilities_table.pkl")
+    translation_table_prev = translation_table_prev["data"]
 
     # translation_table_prev = engine(dutch_sentences,english_sentences,max_iterations,0.0001,"","",False,False)
 
-    for i in range(5): #Convergence loop running it for 5 iterations for obvious reasons
+    for i in range(2): #Convergence loop running it for 2 iterations for obvious reasons
         count = defaultdict(float)
         total = defaultdict(float)
         count_a = defaultdict(float)
@@ -21,11 +22,59 @@ def engine2(dutch_sentences, english_sentences,max_iterations = 5):
         s_total = defaultdict(float)
         for english_sentence, dutch_sentence in zip(english_sentences,dutch_sentences):
             le = len(english_sentence)
-            lf = len(dutch_sentence)
-            for (j,e) in enumerate(le,1):
+            lf = len(dutch_sentence)  
+            #Compute normalization   
+            for (j,e) in enumerate(english_sentence.split(),1):
                 s_total[e] = 0
-                for (i,f) in enumerate(lf,1):
+                for (i,f) in enumerate(dutch_sentence.split(),1):
                     a_dict[(i,j,le,lf)] = 1.0*(1/(lf+1))
-                    s_total[e] += translation_table_prev
+                    s_total[e] += translation_table_prev[f][e]*a_dict[(i,j,le,lf)]
+            print('Normalization')
+            for (j,e) in enumerate(english_sentence.split(),1):
+                for (i,f) in enumerate(dutch_sentence.split(),1):
+                    a_dict[(i,j,le,lf)] = 1.0*(1/(lf+1))
+                    c = (translation_table_prev[f][e]*a_dict[(i,j,le,lf)])/s_total[e]
+                    count[(e,f)] += c
+                    total[f] += c
+                    count_a[(i,j,le,lf)] += c
+                    total_a[(j,le,lf)] += c
+            print('Cnt')
 
+        #estimate probabilites
+        final_translation_prob = defaultdict(float)
+        final_alignment_prob = defaultdict(float)
+        for (e,f) in count.keys():
+            final_translation_prob[(e,f)] = count[(e,f)]/total[f]
+
+        for i,j,le,lf in count_a.keys():
+            final_alignment_prob[(i,j,le,lf)] = count_a[(i,j,le,lf)]/total_a[(j,le,lf)]
+
+    return final_alignment_prob,final_translation_prob   
+
+
+dutch_sentences = unpickle("datasets/dutch/dutch_1p_5t.reduced.pkl")
+dutch_sentences = dutch_sentences[:5]
+english_sentences = unpickle("datasets/english/english_1p_5t.reduced.pkl")
+english_sentences = english_sentences[:5]
+
+
+a,b = engine2(dutch_sentences,english_sentences)
+
+def handle_alignment(translation_prob, alignment_prob,english_sentence,dutch_sentence):
+    translation_ans = defaultdict(float)
+    l_e = len(english_sentence)
+    l_f = len(dutch_sentence)
+    for (j,e) in enumerate(english_sentence.split(),1):
+        cur_max = (0,-1)
+        for (i,f) in enumerate(dutch_sentence.split(),1):
+            val = translation_prob[(e,f)]*alignment_prob[(i,j,l_e,l_f)]
+            if cur_max[1] < val:
+                cur_max = (i,val)
+        translation_ans[j] = cur_max[0]
+    return translation_ans
+
+
+
+
+    # print(handle_alignment(b,a,es,ds),es,'1',ds)
 
--- translate.py
+++ translate.py
@@ -1,15 +1,17 @@
 import argparse
 from unpickle import unpickle
-from typing import List, Dict
-from preprocess import get_sentences_from_document, clean_sentences, printv
+from typing import List
+from preprocess import get_sentences_from_document, clean_sentences
 
-def translate(dutch_sentence: str, translation_table: Dict[str, Dict[str, float]]) -> str:
+def translate(dutch_sentence: str) -> str:
     # NOTE: We will assume a linear correlation/alignment between
     # English words and Dutch word since both have similar origins.
+    translation_table = unpickle("translation_probabilities_table.pkl")
     english_words = []  # type: List[str]
     for word in dutch_sentence.split():
         try:
-            entry = translation_table[word]
+            entry = translation_table["data"][word]
+            print((entry))
             english_estimate = sorted(entry.items(), key=lambda entry: entry[1])[-1][0]
             english_words.append(english_estimate)
         except KeyError:
@@ -21,23 +23,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("document", help="The text file containing dutch sentences to translate. Each sentence must occur on a new line.")
     parser.add_argument("-o", "--output", help="The file to write the output to.")
-    parser.add_argument("-v", "--verbose", help="Enable verbose mode.")
     args = parser.parse_args()
-    verbose = args.verbose
-    printv("Getting and normalizing sentences to translate... ", verbose, end="")
     raw_sentences = get_sentences_from_document(args.document)
     normalized_sentences = clean_sentences(raw_sentences, keep_numbers=True)
-    printv("Done.", verbose)
-    printv("Loading translations probability table... ", verbose, end="")
-    unpickled_data = unpickle("translation_probabilities_table.pkl")
-    assert(isinstance(unpickled_data, Dict))
-    translation_table = unpickled_data["data"]
-    assert(isinstance(translation_table, Dict))
-    printv("Done.", verbose)
-    printv("Translating sentences... ", verbose, end="")
-    translated_sentences = [translate(sentence, translation_table) for sentence in normalized_sentences]
-    printv("Done.", verbose)
-    printv("Writing results to file... ", verbose, end="")
+    translated_sentences = [translate(sentence) for sentence in normalized_sentences]
     if args.output:
         with open(args.output, "w+") as f:
             for sentence in translated_sentences:
@@ -45,4 +34,3 @@ if __name__ == "__main__":
     else:
         for sentence in translated_sentences:
             print(sentence + "\n", end=".")
-    printv("Done.", verbose)
